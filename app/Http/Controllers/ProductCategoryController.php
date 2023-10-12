ed820086f94c1b5b510270364f0369028a6aaa6f
Abdullah hamidy <abdullahhamidy@gmail.com>
2023-10-12 15:47:52 +0430

    product category done

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -4,15 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\ProductCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductCategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        try {
+            $query = new ProductCategory();
+            $searchCol = ['name', 'created_at'];
+            $query = $this->search($query, $request, $searchCol);
+            $trashTotal = clone $query;
+            $trashTotal = $trashTotal->onlyTrashed()->count();
+
+            $allTotal = clone $query;
+            $allTotal = $allTotal->count();
+            if ($request->tab == 'trash') {
+                $query = $query->onlyTrashed();
+            }
+            $query = $query->latest()->paginate($request->itemPerPage);
+            $results = collect($query->items());
+            $total = $query->total();
+
+            return response()->json(["data" => $results,'total' => $total,  "extraTotal" => ['products' => $allTotal, 'trash' => $trashTotal]]);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
     }
 
     /**
@@ -28,7 +48,24 @@ class ProductCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->storeValidation($request);
+
+        // return $request->all();
+        try {
+            DB::beginTransaction();
+            $category = new ProductCategory();
+            // $attributes = $request->only($category->getFillable());
+            // $attributes['status'] = 'false';
+            $category->create([
+                'name' => $request->name,
+                'status' => 'true',
+            ]);
+            DB::commit();
+            return response()->json($category, 201);
+        } catch (\Exception $th) {
+            DB::rollBack();
+            return response()->json($th->getMessage(), 500);
+        }
     }
 
     /**
@@ -58,8 +95,73 @@ class ProductCategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ProductCategory $productCategory)
+    public function destroy(Int $id)
     {
-        //
+
+        try {
+            DB::beginTransaction();
+            $ids  = explode(",", $id);
+            $result = ProductCategory::whereIn("id", $ids)->delete();
+            DB::commit();
+            return response()->json($result, 206);
+        } catch (\Exception $th) {
+            //throw $th;
+            DB::rollBack();
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
+    public function restore(string $id)
+    {
+        try {
+            $ids = explode(",", $id);
+            ProductCategory::whereIn('id', $ids)->withTrashed()->restore();
+            return response()->json(true, 203);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
+    }
+
+    public function forceDelete(string $id)
+    {
+        try {
+            DB::beginTransaction();
+            $ids = explode(",", $id);
+            ProductCategory::whereIn('id', $ids)->withTrashed()->forceDelete();
+            DB::commit();
+            return response()->json(true, 203);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json($th->getMessage(), 400);
+        }
+    }
+
+    public function storeValidation($request)
+    {
+        return $request->validate(
+            [
+                'name' => 'required',
+            ],
+            [
+                'name.required' => "اسم کتگوری ضروری میباشد",
+
+            ]
+
+        );
+    }
+
+    public function changeStatus(Request $request)
+    {
+        try {
+            $status = $request->status;
+            if ($status == false) {
+                $product = ProductCategory::where('id', $request->id)->update(['status'  => true]);
+            } else {
+                $product = ProductCategory::where('id', $request->id)->update(['status'  => false]);
+            }
+            return response()->json($product, 202);
+        } catch (\Throwable $th) {
+            return response()->json($th->getMessage(), 500);
+        }
     }
 }
---
+++ routes/api/product_category_routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProductCategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('product-category', ProductCategoryController::class);
+Route::post('restore-product-category/{id}', [ProductCategoryController::class, 'restore']);
+Route::post('product-category-status', [ProductCategoryController::class, 'changeStatus']);
+Route::delete('force-delete-product-category/{id}', [ProductCategoryController::class, 'forceDelete']);
+
+
